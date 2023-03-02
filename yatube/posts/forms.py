658ed3d31d5822cb1fa065c262a864011a6f0584
Наталья Ляховицкая <momtheprogram@gmail.com>
2023-03-02 20:10:56 +0300

    after flake8

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -7,3 +7,4 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('group', 'text')
+        
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -22,5 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls')),
     path('auth/', include('django.contrib.auth.urls')),
-    
 ]
